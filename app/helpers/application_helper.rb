65664aabb738ec7eb3f22b8248119ff064fe6e07
Valentin Zaborov <xerrox@yandex.ru>
2022-01-17 21:34:11 +0300

    Add new user form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,20 +3,20 @@ module ApplicationHelper
     user.avatar_url.presence || asset_pack_path('media/images/avatar.jpg')
   end
 
-  def inclination(number, one, some, many)
-    modulo100 = number % 100
-    modulo = number % 10
+  # def inclination(number, one, some, many)
+  #   modulo100 = number % 100
+  #   modulo = number % 10
 
-    return many if (11..14).include?(modulo100)
+  #   return many if (11..14).include?(modulo100)
 
-    case modulo
-    when 1
-      one
-    when 2..4
-      some
-    else
-      many
-    end
-  end
-  puts "#{@questions.size} #{inclination(how_many, 'вопрос', 'вопроса', 'вопрсов')}!"
+  #   case modulo
+  #   when 1
+  #     one
+  #   when 2..4
+  #     some
+  #   else
+  #     many
+  #   end
+  # end
+  # puts "#{@questions.size} #{inclination(how_many, 'вопрос', 'вопроса', 'вопрсов')}!"
 end
