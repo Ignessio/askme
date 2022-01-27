8a99c2e6b1297c5e6885f86735b53ccee826cf55
Valentin <xerrox@yandex.ru>
2022-01-27 22:46:34 +0300

    Changed migration with default color to user

--- db/migrate/20220125154328_add_default_color_to_user.rb
+++ db/migrate/20220125154328_add_default_color_to_user.rb
@@ -1,5 +1,5 @@
 class AddDefaultColorToUser < ActiveRecord::Migration[6.1]
-  def self.up
-    change_column :users, :color, :string, :default => "#005a55"
+  def change
+    change_column :users, :color, :string, default: "#005a55"
   end
 end
